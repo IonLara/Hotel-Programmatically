5ecebed09281701553d379ad9d9f2c7843675cbd
SebasMac <ionrodriguezlara@gmail.com>
2023-05-09 21:04:44 -0700

    First Commit

---
+++ Hotel-Programatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel-Programatically
+//
+//  Created by Ion Sebastian Rodriguez Lara on 09/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
